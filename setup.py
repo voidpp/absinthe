b926b252ac4070b7b29c788e91d276efa244f5a0
Lajos Santa <santa.lajos@coldline.hu>
2015-11-20 17:10:57 +0100

    Optional argument for create the specified file(s) if not exists

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Absinthe",
-    version = '1.0.0',
+    version = '1.1.0',
     description = "A tiny server(-client) app to open sshfs connected files over ssh in any local editor",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
